a075bce63032ce050a8812e96e385f7eea427f2e
Heike Winkelvoß <public@egladil.de>
2020-06-14 10:27:52 +0200

    gui für zugang zu Unterlagen

--- frontend/minikaenguru-ws/apps/mkv-app/src/app/teilnahmen/wettbewerb/wettbewerb-info.component.ts
+++ frontend/minikaenguru-ws/apps/mkv-app/src/app/teilnahmen/wettbewerb/wettbewerb-info.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TeilnahmenFacade } from '../teilnahmen.facade';
+import { AuthService } from '@minikaenguru-ws/common-auth';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'mkv-wettbewerb-info',
@@ -10,10 +12,16 @@ export class WettbewerbInfoComponent implements OnInit {
 
 
 	aktuellerWettbewerb$ = this.teilnahmenFacade.aktuellerWettbewerb$;
+	isLoggedIn$ = this.authService.isLoggedIn$;
 
-	constructor(private teilnahmenFacade: TeilnahmenFacade) { }
+
+	constructor(private teilnahmenFacade: TeilnahmenFacade, private authService: AuthService, private router: Router) { }
 
 	ngOnInit(): void {
+	}
+
+	gotoDashboard(): void {
+		this.router.navigateByUrl('/dashboard');
 	}
 
 }
