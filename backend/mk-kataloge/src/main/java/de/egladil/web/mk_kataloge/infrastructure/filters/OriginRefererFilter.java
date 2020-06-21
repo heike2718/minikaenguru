420f62d6223433d82bbe37cf6d9e3ef3581aa7f4
Heike Winkelvoß <public@egladil.de>
2020-06-21 07:10:46 +0200

    injected conf in providers moved

--- backend/mk-kataloge/src/main/java/de/egladil/web/mk_kataloge/infrastructure/filters/OriginRefererFilter.java
+++ backend/mk-kataloge/src/main/java/de/egladil/web/mk_kataloge/infrastructure/filters/OriginRefererFilter.java
@@ -10,6 +10,8 @@ import java.util.List;
 
 import javax.annotation.Priority;
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.PreMatching;
@@ -17,7 +19,12 @@ import javax.ws.rs.core.NoContentException;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.Provider;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import de.egladil.web.commons_net.utils.CommonHttpUtils;
+import de.egladil.web.mk_kataloge.infrastructure.config.ConfigService;
 
 /**
  * OriginRefererFilter
@@ -30,11 +37,10 @@ public class OriginRefererFilter implements ContainerRequestFilter {
 
 	private static final List<String> NO_CONTENT_PATHS = Arrays.asList(new String[] { "/favicon.ico" });
 
-	@ConfigProperty(name = "block.on.missing.origin.referer", defaultValue = "false")
-	boolean blockOnMissingOriginReferer;
+	private static final Logger LOG = LoggerFactory.getLogger(OriginRefererFilter.class);
 
-	@ConfigProperty(name = "target.origin")
-	String targetOrigin;
+	@Inject
+	ConfigService configService;
 
 	@Override
 	public void filter(final ContainerRequestContext requestContext) throws IOException {
@@ -47,8 +53,66 @@ public class OriginRefererFilter implements ContainerRequestFilter {
 			throw new NoContentException(pathInfo);
 		}
 
-		// new OriginRefererFilterDelegate(blockOnMissingOriginReferer,
-		// targetOrigin).validateOriginAndRefererHeader(requestContext);
+		this.validateOriginAndRefererHeader(requestContext);
 	}
 
+	private void validateOriginAndRefererHeader(final ContainerRequestContext requestContext) throws IOException {
+
+		final String origin = requestContext.getHeaderString("Origin");
+		final String referer = requestContext.getHeaderString("Referer");
+
+		LOG.debug("Origin = [{}], Referer=[{}]", origin, referer);
+
+		if (StringUtils.isBlank(origin) && StringUtils.isBlank(referer)) {
+
+			final String details = "Header Origin UND Referer fehlen";
+
+			if (configService.isBlockOnMissingOriginReferer()) {
+
+				logErrorAndThrow(details, requestContext);
+			}
+		}
+
+		if (!StringUtils.isBlank(origin)) {
+
+			checkHeaderTarget(origin, requestContext);
+		}
+
+		if (!StringUtils.isBlank(referer)) {
+
+			checkHeaderTarget(referer, requestContext);
+		}
+	}
+
+	private void checkHeaderTarget(final String headerValue, final ContainerRequestContext requestContext) throws IOException {
+
+		final String extractedValue = CommonHttpUtils.extractOrigin(headerValue);
+
+		if (extractedValue == null) {
+
+			return;
+		}
+
+		String targetOrigin = configService.getTargetOrigin();
+
+		if (targetOrigin != null) {
+
+			List<String> allowedOrigins = Arrays.asList(targetOrigin.split(","));
+
+			if (!allowedOrigins.contains(extractedValue)) {
+
+				final String details = "targetOrigin != extractedOrigin: [targetOrigin=" + targetOrigin
+					+ ", extractedOriginOrReferer="
+					+ extractedValue + "]";
+				logErrorAndThrow(details, requestContext);
+			}
+		}
+	}
+
+	private void logErrorAndThrow(final String details, final ContainerRequestContext requestContext) throws IOException {
+
+		final String dump = CommonHttpUtils.getRequestInfos(requestContext);
+		LOG.warn("Possible CSRF-Attack: {} - {}", details, dump);
+		throw new WebApplicationException("Keine Berechtigung", 401);
+	}
 }
