78b7649d9b76a79336ea1fde1adabea73c9c2506
Heike Winkelvoß <public@egladil.de>
2020-07-26 08:26:13 +0200

    version 0.2.0 fertig

--- frontend/minikaenguru-ws/apps/mkv-app/src/environments/environment.prod.ts
+++ frontend/minikaenguru-ws/apps/mkv-app/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
 	assetsUrl: 'mkv-app/assets',
     apiUrl: 'https://mathe-jung-alt.de/mk-gateway',
     storageKeyPrefix: 'mkv_',
-	version: '0.1.0',
+	version: '0.2.0',
 	consoleLogActive: false,
 	serverLogActive: true,
 	loglevel: 4
