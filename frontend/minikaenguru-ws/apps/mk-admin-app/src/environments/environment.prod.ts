85ba8aa4c4660e9d8f38233a71c040f84b9519cf
Heike Winkelvoß <public@egladil.de>
2020-09-01 22:12:36 +0200

    releasenotes, bugfix NPE angemeldet durch

--- frontend/minikaenguru-ws/apps/mk-admin-app/src/environments/environment.prod.ts
+++ frontend/minikaenguru-ws/apps/mk-admin-app/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
 	assetsUrl: 'https://mathe-jung-alt.de/mk-admin-app/assets',
     apiUrl: 'https://mathe-jung-alt.de/mk-gateway/admin',
     storageKeyPrefix: 'mka_',
-	version: '0.2.4',
+	version: '0.2.6',
 	consoleLogActive: false,
 	serverLogActive: true,
 	loglevel: 4
