1a5b5f6d1bdb82a30b74b215cc81288e3719b166
Heike Winkelvoß <public@egladil.de>
2020-09-03 11:15:58 +0200

    neue version, releasenotes, bug schule-cards

--- frontend/minikaenguru-ws/apps/mkv-app/src/app/lehrer/schulen/schule-card/schule-card.component.ts
+++ frontend/minikaenguru-ws/apps/mkv-app/src/app/lehrer/schulen/schule-card/schule-card.component.ts
@@ -1,25 +1,42 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { Schule } from '../schulen.model';
 import { Router } from '@angular/router';
 import { LehrerFacade } from '../../lehrer.facade';
+import { WettbewerbFacade } from '../../../wettbewerb/wettbewerb.facade';
+import { Wettbewerb } from '../../../wettbewerb/wettbewerb.model';
+import { Subscription } from 'rxjs';
 
 @Component({
 	selector: 'mkv-schule-card',
 	templateUrl: './schule-card.component.html',
 	styleUrls: ['./schule-card.component.css']
 })
-export class SchuleCardComponent implements OnInit {
+export class SchuleCardComponent implements OnInit, OnDestroy {
 
 	devMode = !environment.production;
 
 	@Input()
 	schule: Schule;
 
+	wettbewerb: Wettbewerb;
 
-	constructor(private router: Router, private lehrerFacade: LehrerFacade) { }
+	private aktuellerWettbewerbSubscription: Subscription;
+
+	constructor(private router: Router, private lehrerFacade: LehrerFacade, private wettbewerbFacade: WettbewerbFacade) { }
 
 	ngOnInit(): void {
+
+		this.aktuellerWettbewerbSubscription = this.wettbewerbFacade.aktuellerWettbewerb$.subscribe(
+			w => this.wettbewerb = w
+		);
+
+	}
+
+	ngOnDestroy(): void {
+		if (this.aktuellerWettbewerbSubscription) {
+			this.aktuellerWettbewerbSubscription.unsubscribe();
+		}
 	}
 
 
