b5cf8781b252bb41bbc164cab243827ddcbb951b
Heike Winkelvoß <public@egladil.de>
2020-12-20 16:21:48 +0100

    version and release notes

--- frontend/minikaenguru-ws/build-env/mk-admin-app/environment-a300.ts
+++ frontend/minikaenguru-ws/build-env/mk-admin-app/environment-a300.ts
@@ -9,7 +9,7 @@ export const environment = {
         assetsUrl: 'http://mk-admin-app/assets',
         apiUrl: 'http://a300/mk-gateway/admin',
         storageKeyPrefix: 'mka_',
-        version: '1.2.0',
+        version: '1.2.1',
         consoleLogActive: true,
         serverLogActive: false,
         loglevel: 1
--- frontend/minikaenguru-ws/build-env/mkv-app/environment-a300.ts
+++ frontend/minikaenguru-ws/build-env/mkv-app/environment-a300.ts
@@ -9,7 +9,7 @@ export const environment = {
   apiUrl: 'http://a300/mk-gateway',
   profileUrl: 'http://a300profil-app',
   storageKeyPrefix: 'mkv_',
-  version: '1.2.0',
+  version: '1.2.1',
   consoleLogActive: true,
   serverLogActive: false,
   loglevel: 1
