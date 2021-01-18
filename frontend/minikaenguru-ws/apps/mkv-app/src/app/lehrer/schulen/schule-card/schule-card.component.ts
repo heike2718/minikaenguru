5a37fd322e833797e32ab6519ac77a73ea8fd99a
Heike Winkelvoß <public@egladil.de>
2021-01-18 18:14:01 +0100

    anmelden-button auf schule-card

--- frontend/minikaenguru-ws/apps/mkv-app/src/app/lehrer/schulen/schule-card/schule-card.component.ts
+++ frontend/minikaenguru-ws/apps/mkv-app/src/app/lehrer/schulen/schule-card/schule-card.component.ts
@@ -4,13 +4,15 @@ import { Schule } from '../schulen.model';
 import { Router } from '@angular/router';
 import { LehrerFacade } from '../../lehrer.facade';
 import { WettbewerbFacade } from '../../../wettbewerb/wettbewerb.facade';
+import { User, AuthService } from '@minikaenguru-ws/common-auth';
+import { Subscription } from 'rxjs';
 
 @Component({
 	selector: 'mkv-schule-card',
 	templateUrl: './schule-card.component.html',
 	styleUrls: ['./schule-card.component.css']
 })
-export class SchuleCardComponent implements OnInit {
+export class SchuleCardComponent implements OnInit, OnDestroy {
 
 	devMode = environment.envName === 'DEV';
 
@@ -19,9 +21,34 @@ export class SchuleCardComponent implements OnInit {
 
 	wettbewerb$ = this.wettbewerbFacade.aktuellerWettbewerb$;
 
-	constructor(private router: Router, private lehrerFacade: LehrerFacade, private wettbewerbFacade: WettbewerbFacade) { }
+	private user: User;
 
-	ngOnInit(): void {}
+	private userSubscription: Subscription;
+
+	constructor(private router: Router,
+		private lehrerFacade: LehrerFacade,
+		private wettbewerbFacade: WettbewerbFacade,
+		private authService: AuthService) { }
+
+	ngOnInit(): void {
+
+		this.userSubscription = this.authService.user$.subscribe(
+			u => this.user = u
+		);
+	}
+
+	ngOnDestroy(): void {
+
+		if (this.userSubscription) {
+			this.userSubscription.unsubscribe();
+		}
+	}
+
+	schuleAnmelden(): void {
+		this.lehrerFacade.selectSchule(this.schule);
+		this.lehrerFacade.schuleAnmelden(this.schule, this.user);
+		this.router.navigateByUrl('/lehrer/schule-dashboard/' + this.schule.kuerzel);
+	}
 
 	selectSchule(): void {
 		this.lehrerFacade.selectSchule(this.schule);
