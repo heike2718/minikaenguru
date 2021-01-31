35d1a6cc185954fc22ac09c3e021adca6a85abb9
Heike Winkelvoß <public@egladil.de>
2021-01-31 17:55:43 +0100

    version für nächstes release

--- frontend/minikaenguru-ws/apps/mk-admin-app/src/environments/environment.prod.ts
+++ frontend/minikaenguru-ws/apps/mk-admin-app/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
 	assetsUrl: 'https://mathe-jung-alt.de/mk-admin-app/assets',
     apiUrl: 'https://mathe-jung-alt.de/mk-gateway/admin',
     storageKeyPrefix: 'mka_',
-	version: '1.3.3',
+	version: '1.4.0',
 	consoleLogActive: false,
 	serverLogActive: true,
 	loglevel: 4
