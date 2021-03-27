1201118f0add53f17f21c4194909671a7f736f3c
Heike Winkelvoß <public@egladil.de>
2021-03-27 09:02:29 +0100

    nächste version

--- frontend/minikaenguru-ws/build-env/mk-admin-app/environment-a300.ts
+++ frontend/minikaenguru-ws/build-env/mk-admin-app/environment-a300.ts
@@ -9,7 +9,7 @@ export const environment = {
         assetsUrl: 'http://mk-admin-app/assets',
         apiUrl: 'http://a300/mk-gateway/admin',
         storageKeyPrefix: 'mka_',
-        version: '1.4.4',
+        version: '1.4.5',
         consoleLogActive: true,
         serverLogActive: false,
         loglevel: 1
