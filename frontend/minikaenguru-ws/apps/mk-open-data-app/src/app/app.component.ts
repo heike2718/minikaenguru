50dcea44e315fe5c500797201782073277d6a95e
Heike Winkelvoß <public@egladil.de>
2021-05-02 09:03:08 +0200

    neue app

---
+++ frontend/minikaenguru-ws/apps/mk-open-data-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mkod-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'mk-open-data-app';
+}
