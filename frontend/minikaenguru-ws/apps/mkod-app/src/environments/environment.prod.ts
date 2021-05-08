42cd617d5932c9d94cb2c5b5b4fab7096e0e2996
Heike Winkelvoß <public@egladil.de>
2021-05-08 09:54:06 +0200

    mkod-app: hello world!

---
+++ frontend/minikaenguru-ws/apps/mkod-app/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+	envName: 'PROD',
+	production: true,
+	assetsUrl: 'https://mathe-jung-alt.de/mkod-app/assets',
+    apiUrl: 'https://mathe-jung-alt.de/mk-gateway',
+    storageKeyPrefix: 'mkod_',
+	version: '1.4.6',
+	consoleLogActive: false,
+	serverLogActive: true,
+	loglevel: 4
+};
