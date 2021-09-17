e0c2d5f453a98b4f1f9a54fb4c54c0a079cc29a1
Heike Winkelvoß <public@egladil.de>
2021-09-17 10:18:56 +0200

    start dev milestone 1.6.0

--- frontend/minikaenguru-ws/apps/mk-admin-app/src/environments/environment.prod.ts
+++ frontend/minikaenguru-ws/apps/mk-admin-app/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
 	assetsUrl: 'https://mathe-jung-alt.de/mk-admin-app/assets',
     apiUrl: 'https://mathe-jung-alt.de/mk-gateway/admin',
     storageKeyPrefix: 'mka_',
-	version: '1.5.3',
+	version: '1.6.0',
 	consoleLogActive: false,
 	serverLogActive: true,
 	loglevel: 4
--- frontend/minikaenguru-ws/apps/mkod-app/src/environments/environment.ts
+++ frontend/minikaenguru-ws/apps/mkod-app/src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
         assetsUrl: '/home/heike/git/minikaenguru/frontend/minikaenguru-ws/apps/mkod-app/src/assets',
         apiUrl: 'http://localhost:9510/mk-gateway',
         storageKeyPrefix: 'mkod_',
-        version: '1.5.3',
+        version: '1.6.0',
         consoleLogActive: true,
         serverLogActive: false,
         loglevel: 1
