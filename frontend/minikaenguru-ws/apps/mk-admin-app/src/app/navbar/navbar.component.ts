2a9f144d5919426125c8cb9f9599117b164d8a68
Heike Winkelvoß <public@egladil.de>
2021-10-17 08:18:51 +0200

    mk-admin layout small fixed

--- frontend/minikaenguru-ws/apps/mk-admin-app/src/app/navbar/navbar.component.ts
+++ frontend/minikaenguru-ws/apps/mk-admin-app/src/app/navbar/navbar.component.ts
@@ -3,13 +3,6 @@ import { AuthService, AuthState, isLoggedIn, isLoggedOut, Session, user } from '
 import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
 import { Store } from '@ngrx/store';
 import { environment } from '../../environments/environment';
-import { AppState } from '../reducers';
-import { resetWettbewerbe } from '../wettbewerbe/+state/wettbewerbe.actions';
-import { resetKataloge } from '../katalogpflege/+state/katalogpflege.actions';
-import { resetVeranstalters } from '../veranstalter/+state/veranstalter.actions';
-import { resetAktuelleMeldung } from '../aktuelle-meldung/+state/aktuelle-meldung.actions';
-import { resetSchulteilnahmen } from '../schulteilnahmen/+state/schulteilnahmen.actions';
-import { dateCleared } from '../eventlog/+state/eventlog.actions';
 import { LogoutService } from '../services/logout.service';
 
 @Component({
