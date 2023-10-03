c493306f766bb4f418beaa88866ae8d1241544ef
Heike Winkelvoß <public@egladil.de>
2023-10-03 11:36:27 +0200

    I0406: script fuer testdaten

---
+++ backend/mk-gateway/sql-testdaten/create-teilnahmen.sql
@@ -0,0 +1,14 @@
+/*
+horst-schneider@egladil.de
+
+cb2d332b-ecea-41d1-8abd-4dde15688e90
+
+Parkschule Weimar  U7B0OYKZ
+
+
+*/
+
+
+insert into TEILNAHMEN (UUID, TEILNAHMEART, TEILNAHMENUMMER, WETTBEWERB_UUID,  SCHULNAME, ANGEMELDET_DURCH) values ('349eb440-f6fa-446d-bb18-cebd044a0308','SCHULE','U7B0OYKZ', '2018', 'Parkschule Weimar', 'cb2d332b-ecea-41d1-8abd-4dde15688e90');
+
+insert into TEILNAHMEN (UUID, TEILNAHMEART, TEILNAHMENUMMER, WETTBEWERB_UUID,  SCHULNAME, ANGEMELDET_DURCH) values ('bc19e793-a625-4701-affc-6a159bc51465','SCHULE','U7B0OYKZ', '2019', 'Parkschule Weimar', 'cb2d332b-ecea-41d1-8abd-4dde15688e90');
