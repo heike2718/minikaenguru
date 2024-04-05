8c9267fe9eae939f2df0ee673e9ce417c991c8f0
Heike Winkelvoß <public@egladil.de>
2024-04-05 16:25:47 +0200

    I0456: refactor move home and shared

--- statistics/frontend/mkbiza-app/src/app/app.routes.ts
+++ statistics/frontend/mkbiza-app/src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './domains/home/home.component';
+import { HomeComponent } from './home/home.component';
 
 export const appRoutes: Routes = [
     {
--- statistics/frontend/mkbiza-app/src/app/shared/layout/jest.config.ts
+++ statistics/frontend/mkbiza-app/src/app/shared/layout/jest.config.ts
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default {
   displayName: 'layout',
-  preset: '../../../../../jest.preset.js',
+  preset: '../../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../../../../coverage/src/app/domains/shared/layout',
+  coverageDirectory: '../../../../coverage/src/app/domains/shared/layout',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
