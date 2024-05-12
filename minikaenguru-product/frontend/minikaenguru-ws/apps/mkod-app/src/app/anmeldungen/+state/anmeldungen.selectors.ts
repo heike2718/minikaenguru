fdf6d836a30ccdc2aeb2d3291246b1fc244b1867
Heike Winkelvoß <public@egladil.de>
2024-05-12 13:58:37 +0200

    mk-I0460: mkod-app has gone

--- minikaenguru-product/frontend/minikaenguru-ws/apps/mkod-app/src/app/anmeldungen/+state/anmeldungen.selectors.ts
+++
@@ -1,6 +0,0 @@
-import { createFeatureSelector, createSelector } from '@ngrx/store';
-import * as fromAnmeldungen from './anmeldungen.reducer';
-
-export const anmeldungenState = createFeatureSelector<fromAnmeldungen.AnmeldungenState>(fromAnmeldungen.anmeldungenFeatureKey);
-
-
