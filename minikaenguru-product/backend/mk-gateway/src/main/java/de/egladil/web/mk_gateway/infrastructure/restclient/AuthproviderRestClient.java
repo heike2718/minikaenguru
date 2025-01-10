66105eb281000280b250a8e53de4d2bf19707a7a
Heike Winkelvoß <public@egladil.de>
2025-01-10 08:57:19 +0100

    I0476: default log dir geändert

--- minikaenguru-product/backend/mk-gateway/src/main/java/de/egladil/web/mk_gateway/infrastructure/restclient/AuthproviderRestClient.java
+++ minikaenguru-product/backend/mk-gateway/src/main/java/de/egladil/web/mk_gateway/infrastructure/restclient/AuthproviderRestClient.java
@@ -1,5 +1,5 @@
 // =====================================================
-// Project: bv-admin
+// Project: mk-gateway
 // (c) Heike Winkelvoß
 // =====================================================
 package de.egladil.web.mk_gateway.infrastructure.restclient;
