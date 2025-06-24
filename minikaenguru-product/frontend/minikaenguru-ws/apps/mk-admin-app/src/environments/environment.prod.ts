b969e36ca08916f0134b1cd69439fcbb29eeaaad
Heike Winkelvoß <public@egladil.de>
2025-06-24 07:25:40 +0200

    Angular 18 (#488)
    * pre-update to Angular 17
    
    * v17 noch nicht fertig - Zwischenschritt 1
    
    * mkv-app, mk-admin-app bumped to Angular 17
    
    * ng-bootstrap bumped to 16
    
    * mkv-app, mk-admin-app bumped to Angular 18

--- minikaenguru-product/frontend/minikaenguru-ws/apps/mk-admin-app/src/environments/environment.prod.ts
+++ minikaenguru-product/frontend/minikaenguru-ws/apps/mk-admin-app/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
     apiUrl: 'https://mathe-jung-alt.de/mk-gateway/admin',
 	profileUrl: 'https://mathe-jung-alt.de/benutzerprofil/',
     storageKeyPrefix: 'mka_',
-	version: '2.7.0',
+	version: '2.7.1',
 	consoleLogActive: false,
 	serverLogActive: true,
 	loglevel: 4
